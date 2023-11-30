f84cfc15e91e7777e5518c75a1fe27b5fe916530
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-11-30 19:58:10 +0100

    added register and login page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
-  return <h1 className="text-4xl">HomeLayout</h1>;
+  return (
+    <>
+      <nav>
+        <span className="text-4xl text-primary">Comfy</span>
+      </nav>
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
+    </>
+  );
 };
 
 export default HomeLayout;
