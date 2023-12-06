4e1a2d75d60b9b3f0224a2526793bc2ac700a71e
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-12-06 12:03:39 +0100

    added error element

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,36 @@ import {
   SingleProduct,
 } from "./pages";
 
+import { ErrorElement } from "./components";
+
+//loaders
+
+import { loader as landingLoader } from "./pages/Landing";
+
+//actions
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
-      { index: true, element: <Landing /> },
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
+      },
+      { path: "products", element: <Products /> },
+      { path: "products/:id", element: <SingleProduct /> },
+      { path: "cart", element: <Cart /> },
       { path: "about", element: <About /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "orders", element: <Orders /> },
     ],
   },
+  { path: "/login", element: <Login />, errorElement: <Error /> },
+  { path: "/register", element: <Register /> },
 ]);
 
 const App = () => {
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { Hero } from "../components";
+import { customFetch } from "../utils";
+const url = "/products?featured=true";
+
+export const loader = async () => {
+  const response = await customFetch(url);
+  const products = response.data.data;
+  console.log(response);
+  return { products };
+};
 
 const Landing = () => {
   return (
