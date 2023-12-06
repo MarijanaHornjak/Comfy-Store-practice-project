ff28526a330968646da4ff1fe77e32afd4dca862
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-12-06 21:55:29 +0100

    added single product

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import { ErrorElement } from "./components";
 //loaders
 
 import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleProductLoader } from "./pages/SingleProduct";
 
 //actions
 
@@ -35,7 +36,12 @@ const router = createBrowserRouter([
         loader: landingLoader,
       },
       { path: "products", element: <Products /> },
-      { path: "products/:id", element: <SingleProduct /> },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
+      },
       { path: "cart", element: <Cart /> },
       { path: "about", element: <About /> },
       { path: "checkout", element: <Checkout /> },
