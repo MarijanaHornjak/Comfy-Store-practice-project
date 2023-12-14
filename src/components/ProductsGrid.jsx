09d4219e662d061d4346365360f5aeafd667f357
MarijanaHornjak <marijanahornjak95@gmail.com>
2023-12-14 19:41:57 +0100

    pagination setup

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -22,7 +22,7 @@ const ProductsGrid = () => {
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title capitalize tracking-wider">{title}</h2>
-              <span className="text-secondary">{dollarsAmount}</span>
+              <span className="text-secondary">${dollarsAmount}</span>
             </div>
           </Link>
         );
